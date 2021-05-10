3a4595823d74a71e7c45d3f96039ff0c7cd8bb05
hg_yellow <jang010505@gmail.com>
2021-05-11 06:12:36 +0900

    끝..? ㄱ새벽 코딩 ㅋㅋ,,,

--- app/src/main/java/com/example/a1week/MainActivity.kt
+++ app/src/main/java/com/example/a1week/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.a1week
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.login).setOnClickListener{
+            val id = findViewById<EditText>(R.id.id)
+            val password = findViewById<EditText>(R.id.password)
+            if (id.text.toString().isEmpty()){
+                Toast.makeText(this,"아이디 혹은 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+                id.requestFocus()
+            }
+            else if (password.text.toString().isEmpty()){
+                Toast.makeText(this,"아이디 혹은 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+                password.requestFocus()
+            }
+            else{
+                findViewById<TextView>(R.id.errorMessages).visibility = View.VISIBLE
+            }
+        }
     }
 }
